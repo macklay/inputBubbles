83ff99a7d4035136d1f924be3c9681fde8755342
a.dergunov <a.dergunov@timeweb.ru>
2015-06-10 11:57:45 +0300

    Release 0.9.2

--- dist/js/input-bubbles.js
+++ dist/js/input-bubbles.js
@@ -402,49 +402,51 @@
 })();
 
 (function() {
-    if (typeof $ === 'undefined') {
-        return;
-    }
-
-    if (typeof window.inputBubbles === 'undefined') {
-        throw new Error('InputBubbles native was not loaded!');
-    }
-
-    $.fn.inputBubbles = function(options) {
-        if (!this.length) {
+    if (window.jQuery !== 'undefined') {
+        if (typeof $ === 'undefined') {
             return;
         }
 
-        if (typeof options === 'string') {
-            var guid = this.data('input-bundles');
-
-            if (!guid) {
-                throw new Error('Trying to call inputBubble plugins methods on element without initialization!');
-            }
-
-            var instance = window.inputBubbles({
-                id: guid
-            });
-
-            if (typeof instance === 'undefined') {
-                throw new Error('Error! Instance was initialized with errors or was not initialized.');
-            }
-
-            if (arguments.length > 1) {
-                var args = ([]).slice.call(arguments);
-                instance[options].apply(instance, args.splice(1, args.length));
-            } else {
-                instance[options]();
-            }
-            return this;
+        if (typeof window.inputBubbles === 'undefined') {
+            throw new Error('InputBubbles native was not loaded!');
         }
 
+        $.fn.inputBubbles = function(options) {
+            if (!this.length) {
+                return;
+            }
 
-        var _options = options ? options : {};
-        _options.element = this[0];
+            if (typeof options === 'string') {
+                var guid = this.data('input-bundles');
 
-        this.inputBubbles = window.inputBubbles(_options);
+                if (!guid) {
+                    throw new Error('Trying to call inputBubble plugins methods on element without initialization!');
+                }
 
-        return this;
-    };
+                var instance = window.inputBubbles({
+                    id: guid
+                });
+
+                if (typeof instance === 'undefined') {
+                    throw new Error('Error! Instance was initialized with errors or was not initialized.');
+                }
+
+                if (arguments.length > 1) {
+                    var args = ([]).slice.call(arguments);
+                    instance[options].apply(instance, args.splice(1, args.length));
+                } else {
+                    instance[options]();
+                }
+                return this;
+            }
+
+
+            var _options = options ? options : {};
+            _options.element = this[0];
+
+            this.inputBubbles = window.inputBubbles(_options);
+
+            return this;
+        };
+    }
 })();
