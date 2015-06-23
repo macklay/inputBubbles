fdb434ceaa5a2564c2ea330f2d6fdc7a08f932fb
Nick Smirnov <macklay@gmail.com>
2015-06-23 11:31:41 +0300

    Remove prototype from contains array function
    prototyped contains function create problems for forin loops in all other code

--- src/js/cursor-manager.js
+++ src/js/cursor-manager.js
@@ -5,10 +5,10 @@
     var voidNodeTags = ['AREA', 'BASE', 'BR', 'COL', 'EMBED', 'HR', 'IMG', 'INPUT', 'KEYGEN', 'LINK', 'MENUITEM', 'META', 'PARAM', 'SOURCE', 'TRACK', 'WBR', 'BASEFONT', 'BGSOUND', 'FRAME', 'ISINDEX'];
 
     //From: http://stackoverflow.com/questions/237104/array-containsobj-in-javascript
-    Array.prototype.contains = function(obj) {
-        var i = this.length;
+    var contains = function(arr, obj) {
+        var i = arr.length;
         while (i--) {
-            if (this[i] === obj) {
+            if (arr[i] === obj) {
                 return true;
             }
         }
@@ -18,7 +18,7 @@
     //Basic idea from: http://stackoverflow.com/questions/19790442/test-if-an-element-can-contain-text
     function canContainText(node) {
         if(node.nodeType == 1) { //is an element node
-            return !voidNodeTags.contains(node.nodeName);
+            return !contains(voidNodeTags, node.nodeName);
         } else { //is not an element node
             return false;
         }
